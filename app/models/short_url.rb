b52f6538d5fb70f3289f6982cc35bb38d8c6fce5
Warren Chaudhry <warrenchaudhry@gmail.com>
2022-03-01 21:59:42 +0800

    added some validations

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,6 +2,7 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
+  validates :full_url, uniqueness: { case_sensitive: false }, presence: true
   validate :validate_full_url
 
   def short_code
@@ -13,6 +14,13 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    return unless full_url.present?
+
+    errors.add(:full_url, 'is not a valid url') unless valid_url?
   end
 
+  def valid_url?
+    url = URI.parse(full_url) rescue false
+    url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
+  end
 end
